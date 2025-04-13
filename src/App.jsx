a71d79703eb7657210252438ffb457bdfd7088d3
F15 <duongquocthai368@gmail.com>
2025-04-14 03:02:33 +0700

    learn about render list

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@ import Header from './Header.jsx'
 import Food from './Food.jsx'
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
+import List from './List.jsx'
 
 function App() {
+  const fruits = [{id:1,name:"apple",cali:99},
+                    {id:2,name:"orange",cali:34},
+                    {id:3,name:"banana",cali:194},
+                    {id:4,name:"coconut",cali:534},
+                    {id:5,name:"pineapple",cali:45}]
 
+  const vegetables = [{id:1,name:"potatoes",cali:9},
+                      {id:2,name:"celery",cali:34},
+                      {id:3,name:"carrots",cali:53},
+                      {id:4,name:"corn",cali:4},
+                      {id:5,name:"broccoli",cali:33}]
+
+    
   return(
+    
     <>
       <Header/>
-      <Card/><Card/><Card/><Card/><Card/><Card/>
+      <List items={fruits} category="Fruits"/>
+      <List items={vegetables} category="vegetables"/>
+      {/* <Card/> */}
+
       <Footer/>
       
     </>
---
+++ src/List.jsx
@@ -0,0 +1,37 @@
+
+function List(props){
+    // const fruits = ["apple","orange","banana","coconut","pineapple"]
+    // const fruits = [{id:1,name:"apple",cali:99},
+    //                 {id:2,name:"orange",cali:34},
+    //                 {id:3,name:"banana",cali:194},
+    //                 {id:4,name:"coconut",cali:534},
+    //                 {id:5,name:"pineapple",cali:45}]
+
+    // const vegetables = ["potatoes","celery","carrots","corn","broccoli"]
+    
+    
+    // fruits.sort((a,b) => a.name.localeCompare(b.name));
+    // fruits.sort((a,b) => b.name.localeCompare(a.name));
+    // fruits.sort((a,b) => a.cali - b.cali);
+    // fruits.sort((a,b) => b.cali - a.cali);
+
+
+    //use filter (instead name fruit to fruitFilter if use filter)
+    //const fruitFilter = fruits.filter(fruit => fruit.cali < 100);
+
+
+    const FruitsList = props.items;
+
+    const itemFruit = FruitsList.map(fruit => <li key={fruit.id}>Name:{fruit.name}&nbsp;{fruit.cali}</li>)
+
+    return( 
+        <>
+        <h3 className="category-list">{props.category}</h3>
+        <ol className="items-list">{itemFruit}</ol> 
+        </>
+        );
+
+}
+
+
+export default List
